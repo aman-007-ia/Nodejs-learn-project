c61327bb52886bf8e88b0ef4c3dfd3b5c361b317
aman-007-ia <amanyadav794172@gmail.com>
2026-05-29 14:33:03 +0530

    module in nodejs

---
+++ hello-world/hello.js
@@ -0,0 +1,8 @@
+// const {add,subtract} = require('./math');
+const math = require('./math');
+
+console.log("Hello world");
+// console.log(add(2, 3));
+// console.log(subtract(5, 3));
+console.log(math.add(2, 3));
+console.log(math.subtract(5, 3));
